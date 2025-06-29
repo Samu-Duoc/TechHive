8b609adc1221d688fd8a5c5da0f73713860a84ca
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-06-28 21:02:16 -0400

    Microservicios en proceso

---
+++ Microservicios_5/com.TechHive_Pedido_Pagos/src/main/java/com/TechHive_Pedido_Pagos/Application.java
@@ -0,0 +1,13 @@
+package com.TechHive_Pedido_Pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
